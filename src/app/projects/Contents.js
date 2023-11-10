dd9f1e3a751cf5b34c054afc7cbfac376dce8c74
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2023-11-10 08:34:53 +0100

    feat: add projects page

---
+++ src/app/projects/Contents.js
@@ -0,0 +1,8 @@
+"use client"
+
+import { MarkdownContainer } from '../../lib'
+import Content from './contents.mdx'
+
+export function Contents() {
+  return <MarkdownContainer content={Content} />
+}
---
+++ src/app/projects/page.js
@@ -0,0 +1,9 @@
+import { Contents } from './Contents.js'
+
+export const metadata = {
+  title: 'My work',
+}
+
+export default function WayOfWorking() {
+  return <Contents />
+}
